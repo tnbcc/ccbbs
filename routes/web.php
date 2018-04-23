7e5cdc83bf17070ca2d501d45c387c57160c4ab3
nbc <tniub.cc@gmail.com>
2018-04-23 11:46:35 +0000

    完成auth

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,23 @@
 
 //首页
 Route::get('/','PagesController@root')->name('root');
+
+//Auth::routes();
+
+
+Route::namespace('Auth')->group(function(){
+  //登录路由
+  Route::get('login','LoginController@showLoginForm')->name('login');
+  Route::post('login','LoginController@login');
+  Route::post('logout', 'LoginController@logout')->name('logout');
+
+  //注册路由
+  Route::get('register','RegisterController@showRegistrationForm')->name('register');
+  Route::post('register','RegisterController@register');
+
+  //重置密码
+  Route::get('password/reset','ForgotPasswordController@showLinkRequestForm')->name('password.request');
+  Route::post('password/email','ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+  Route::get('password/reset/{token}','ResetPasswordController@showResetForm')->name('password.reset');
+  Route::post('password/reset','ResetPasswordController@reset');
+});
