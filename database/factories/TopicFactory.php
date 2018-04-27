103f3e62f394b5d2d87899f0b583395122c05feb
nbc <tniub.cc@gmail.com>
2018-04-27 12:19:34 +0000

    话题表填充假数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,19 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+  //随机生成小段落
+  $sentence = $faker->sentence();
+  //随机获取一个月之内的时间
+  $updated_at = $faker->dateTimeThisMonth();
+  //创建时间不能晚于修改时间
+  $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+         'title' => $sentence,
+         'body' => $faker->text(),
+         'excerpt' => $sentence,
+         'created_at' => $created_at,
+         'updated_at' => $updated_at,      
     ];
 });
