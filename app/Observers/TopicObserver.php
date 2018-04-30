54783aaa2d23f06677a94cad14fc8ba422f45ede
nbc <tniub.cc@gmail.com>
2018-04-30 13:42:16 +0000

    预防XSS攻击

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -12,6 +12,8 @@ class TopicObserver
     //生成话题摘录使用模型监控器在话题创建前写进数据库
     public function saving(Topic $topic)
     {
+       //防止XSS攻击
+       $topic->body = clean($topic->body, 'user_topic_body');
        $topic->excerpt = make_excerpt($topic->body);
     }
 }
